f89b1c603b27cd87b38769af5e3b8fa626414f8a
mariadonati07 <00001092743546sp@al.educacao.sp.gov.br>
2024-11-24 22:02:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisica',
+    'Formulda para se calcular força?',
+    'F = M.A'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Historia',
+    'O que é foi a revolução industrial?',
+    ' um período de grande desenvolvimento tecnológico que teve início na Inglaterra no século XVIII e se espalhou pelo mundo'
+)
+
+criaCartao(
+    'comida',
+    'Como se faz um ovo frito?',
+    'Fritando ele'
+)
